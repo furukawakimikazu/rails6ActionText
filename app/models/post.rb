46b1159d833d8b6c7ad802ff6cd90b7b32b936fc
furukawakimikazu <g.guquan@gmail.com>
2020-01-10 16:18:35 +0900

    content length validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,21 @@
 class Post < ApplicationRecord
   has_rich_text :content
   validates :title, length: { maximum: 32 }, presence: true
+
+  validate :validate_content_length
+
+  MAX_CONTENT_LNGTH = 50
+
+
+  def validate_content_length
+    length =  content.to_plain_text.length
+    if length > MAX_CONTENT_LNGTH
+      errors.add(
+        :content,
+        :too_long,
+        max_content_length: MAX_CONTENT_LNGTH,
+        length: length
+      )
+    end
+  end
 end
